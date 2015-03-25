57946f323cc278e6cef513648447abc9bcc39867
diarmuid <contact@diarmuid.ie>
2015-03-25 21:39:24 +0000

    Remove unused methods

--- src/Utilities/BaseConvert.php
+++ src/Utilities/BaseConvert.php
@@ -92,38 +92,4 @@ class BaseConvert
     {
         return preg_split('//u', $string, -1, PREG_SPLIT_NO_EMPTY);
     }
-
-    public static function oldmethod($numberInput, $fromBaseInput, $toBaseInput)
-    {
-        if ($fromBaseInput==$toBaseInput) {
-            return $numberInput;
-        }
-        $fromBase = str_split($fromBaseInput, 1);
-        $toBase = str_split($toBaseInput, 1);
-        $number = str_split($numberInput, 1);
-        $fromLen=strlen($fromBaseInput);
-        $toLen=strlen($toBaseInput);
-        $numberLen=strlen($numberInput);
-        $retval='';
-        if ($toBaseInput == '0123456789') {
-            $retval=0;
-            for ($i = 1; $i <= $numberLen; $i++) {
-                $retval = bcadd($retval, bcmul(array_search($number[$i-1], $fromBase), bcpow($fromLen, $numberLen-$i)));
-            }
-            return $retval;
-        }
-        if ($fromBaseInput != '0123456789') {
-            $base10=self::oldmethod($numberInput, $fromBaseInput, '0123456789');
-        } else {
-            $base10 = $numberInput;
-        }
-        if ($base10<strlen($toBaseInput)) {
-            return $toBase[$base10];
-        }
-        while ($base10 != '0') {
-            $retval = $toBase[bcmod($base10, $toLen)].$retval;
-            $base10 = bcdiv($base10, $toLen, 0);
-        }
-        return $retval;
-    }
 }
